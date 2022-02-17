0046f4add973682bae24b688396db8219f5a7633
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-17 11:01:56 +0100

    Established connection

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
 
+
+try{
+    $connection = new PDO(
+        'mysql:dbname=world;host=localhost;charset=utf8', // connection string
+        'root', // username
+        '' // password
+    );
+} catch (PDOException $e) {
+    echo "Connection failer: " . $e->getMessage();
+}
+
+echo "end";
